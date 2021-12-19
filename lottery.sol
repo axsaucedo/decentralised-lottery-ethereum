72c57500d6704bae246438baefc9de9817a50a7a
Alejandro Saucedo <axsauze@gmail.com>
2021-12-19 08:14:07 +0000

    Added ability to join lottery

--- lottery.sol
+++ lottery.sol
@@ -3,6 +3,19 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery{
     address payable[] public players;
+    address public manager;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    receive() external payable{
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
 }
 
 
